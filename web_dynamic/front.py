fccee5bc3c406a574ddbbc60d3ad209bb8bb015b
Gabriel Cifuentes <gcifuentess@hotmail.com>
2022-01-16 23:41:01 +0000

    add - New Report feature
    New page with the report of all of the registered users in the database
    
    Changes:
    	modified:   web_dynamic/front.py
    	new file:   web_dynamic/static/styles/report.css
    	new file:   web_dynamic/templates/report.html

--- web_dynamic/front.py
+++ web_dynamic/front.py
@@ -2,14 +2,24 @@
 '''Starts a Flask Web Application'''
 from flask import Flask, render_template
 import uuid
+from models import storage
+from models.user import User
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
     '''Coink is alive!'''
-
     return render_template('index.html',
+                           cache_id=uuid.uuid4())
+
+@app.route('/report', strict_slashes=False)
+def report():
+    '''shows all the users registrated'''
+    users = storage.all(User).values()
+
+    return render_template('report.html',
+                           users=users,
                            cache_id=uuid.uuid4())
 
 
