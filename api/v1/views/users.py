e5de53303ebbbf50361f3e4e07d3a12ab5158444
Gabriel Cifuentes <gcifuentess@hotmail.com>
2022-01-17 01:21:36 +0000

    add - New log feature
    when a user is created a log.txt file is updated or created
    
    Changes:
    	modified:   api/v1/views/users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -5,6 +5,7 @@ from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 import re
+import os
 
 
 email_patt = r'[\w\.]{2,30}\+?[\w]{0,10}@[\w\-]+\.[a-z]{2,5}\.?[a-z]{0,5}'
@@ -62,20 +63,41 @@ def post_user():
     except:
         abort(400, description="Not a JSON")
 
-    if 'email' not in data:
-        abort(400, description="email faltante")
     if 'full_name' not in data or data['full_name'] == '':
         abort(400, description="nombre completo faltante")
-    if 'city' not in data or data['city'] == '':
-        abort(400, description="ciudad faltante")
-
+    if ',' in data['full_name']:
+        abort(400, description="el nombre no puede contener comas")
+    if 'email' not in data:
+        abort(400, description="email faltante")
     if re.match(email_patt, data['email']) == None:
         abort(400, description="formato email errado")
+    if 'city' not in data or data['city'] == '':
+        abort(400, description="ciudad faltante")
+    if ',' in data['city']:
+        abort(400, description="la ciudad no puede contener comas")
 
-    instance = User(**data)
+
+    user = User(**data)
     try:
-        instance.save()
+        user.save()
     except:
         abort(400, description="email ya registrado")
 
-    return make_response(jsonify(instance.to_dict()), 201)
+    log_str = "[NEW USER],{},{},{},{},{}\n".format(
+        str(user.created_at),
+        str(user.id),
+        user.full_name,
+        user.email,
+        user.city
+        )
+
+    log_path = "./log.txt"
+
+    if not os.path.exists(log_path):
+        header_str = "log_type,created_at,user_id,full_name,email,city\n"
+        log_str = header_str + log_str
+
+    with open(log_path, mode='a', encoding='utf-8') as a_file:
+        a_file.write(log_str)
+
+    return make_response(jsonify(user.to_dict()), 201)
